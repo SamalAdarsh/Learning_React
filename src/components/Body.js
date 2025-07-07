c5188c135d556c89d1a4e5044c89cac6547e8e17
Adarsh <codewithadarsh007@gmail.com>
2025-07-07 19:40:26 +0530

    added CROS Proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,7 @@ import resList from "../utils/dummyData";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-
-
 const Body = () => {
-  
-
   const [listofRestaurents, setlistofRestaurents] = useState([]);
 
   const [filteredRestaurents, setfilteredRestaurents] = useState([]);
@@ -22,7 +18,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5246091&lng=73.8786239&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5246091&lng=73.8786239&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
@@ -83,8 +79,6 @@ const Body = () => {
         >
           Top Rated Restaurants
         </button>
-
-        
       </div>
 
       <div className="res-conatiner">
